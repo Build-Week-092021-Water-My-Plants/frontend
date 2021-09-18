cd849ed43b9e0ef91e5aeec4e46a39c16d94da07
Rick <ricksmycodeguy@gmail.com>
2021-09-18 18:22:52 -0500

    add PrivateRoute.js

---
+++ src/components/PlantList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PlantList = () => {
+    return (
+        <div>
+            <h1>You Found your Plant List YAAAA!</h1>
+            <h2>Too bad we're still working on it</h2>
+            <h3>Come Back Later</h3>
+        </div>
+    )
+}
+
+export default PlantList
