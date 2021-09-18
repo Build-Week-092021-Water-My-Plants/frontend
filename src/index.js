0b0b0afa9a2c69584e071bc453970eb7d84b45a7
Rick <ricksmycodeguy@gmail.com>
2021-09-18 11:39:43 -0500

    set up index.js with Redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { reducer } from './reducers/index'
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
+  </Provider>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
