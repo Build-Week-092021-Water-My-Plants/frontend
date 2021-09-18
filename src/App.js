a2e7487ea5c5ed47385409d57935f54d7dbca9bf
Rick <ricksmycodeguy@gmail.com>
2021-09-18 17:24:24 -0500

    Index.js Styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
 import PrivateRoute from "./helpers/PrivateRoute";
 // import LoginPage from "./components/pages/Login/LoginContainer";
