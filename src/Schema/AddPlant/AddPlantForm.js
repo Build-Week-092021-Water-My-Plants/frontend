6547bf450abff25cd44d349ab753573de2723996
Rick <ricksmycodeguy@gmail.com>
2021-09-19 18:19:09 -0500

    final push for the day

--- src/Schema/AddPlant/AddPlantForm.js
+++ src/Schema/AddPlant/AddPlantForm.js
@@ -4,13 +4,15 @@ import { useHistory } from "react-router-dom";
 function AddPlantForm() {
     const history = useHistory();
     const onSubmit = (evt) => {
-        history.push("/AddSuccess");
         evt.preventDefault();
         // submit();
+        history.push("/AddSuccess");
     }
     return (
         <div>
+            <form onSubmit={onSubmit}>
             <h1>rendered from addPlantForm.js Lets get some form going on</h1>
+            </form>
         </div>
     )
 }
